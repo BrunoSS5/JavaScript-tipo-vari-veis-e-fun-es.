74d17ba3054004b2fedd3cdc3e8ccb9158a76a40
Bruno Santos <pedrobruno01994@gmail.com>
2022-07-29 16:16:41 -0300

    primeiro commit

---
+++ console.js
@@ -0,0 +1,6 @@
+// log - 
+
+const minhaVar = true;
+console.log(534);
+console.log('EU sou um texto');
+console.log(minhaVar);
---
+++ erros.js
@@ -0,0 +1,4 @@
+const numero = 1;
+
+const minhaVar = '01';
+console.log(numero + minhaVar);
